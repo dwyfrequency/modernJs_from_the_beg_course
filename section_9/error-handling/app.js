b4688031b07996aed91323bf7a1d02b19865cf35
dwyfrequency <jdwy215@gmail.com>
2018-07-13 07:18:26 -0400

    created error handling file

--- section_9/error-handling/app.js
+++ section_9/error-handling/app.js
@@ -1,10 +1,56 @@
 const log = console.log;
 
+const user = {email: 'jdow@gmail.com'};
+
 try {
-  // Produce a referenceError
-  myFunction();
+  // Produce a ReferenceError
+  // myFunction();
+
+  // Produces a TypeError 
+  // null.myFunction(); // cannot call a property of null
+
+  // Produce a SyntaxError
+  // log(eval(`Hello world`));
+
+  // Will produce a URIError
+  // decodeURIComponent('%');
+
+  // // throws our own custom error to the catch block
+  // if (!user.name) {
+  //   throw 'User has no name'; // create your own error. It is just throwing a string
+  // }
+
+  // throws our own custom error to the catch block
+  if (!user.name) {
+    throw new SyntaxError('User has no name'); // we can format like a specific error
+  }
 } catch(e) {
+
+  /* Error handling for myFunction(); */
+  // log(e); //gives you name (ReferenceError) and message (myFunction is not defined) 
+  // log(e.message); // just gives you the msg
+  // log(e.name); // just get the name 
+  // log(e instanceof ReferenceError); // returns bool : true
+  // log(e instanceof TypeError) // returns bool : false 
+
+  /* Error handling for null.myFunction(); */
+  // log(`${e.name}: it's null dawg. Can't do that! `)
+
+  /* Error handling for eval(`Hello world`); */
+  // log(e); // SyntaxError: Unexpected identifier
+
+  /* Error handling for decodeURIComponent('%'); */
+  // log(e); // URIError: URI malformed
+
+  /* Error handling for our throw statement */
+  // log(e); // User has no name
+
+  /* Error handling for throw new*/
   log(e);
+  
+} finally {
+  /* finally block runs no matter what */
+  log('Finally block runs regardless of result...') 
 }
 
-log('Program continues...')
+log('Program continues...');
