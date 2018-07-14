227bf1f16e1a4cd136de073c52285dd733647727
dwyfrequency <jdwy215@gmail.com>
2018-07-14 08:48:28 -0400

    completed iterator and generator notes

--- section_10/iterators-generators/app.js
+++ section_10/iterators-generators/app.js
@@ -25,3 +25,41 @@ const namesArr = ["Jack", "Jill", "John"];
 // init iterator and pass in the names array
 const names = nameIterator(namesArr);
 
+log(names.next());
+log(names.next());
+log(names.next());
+log(names.next());
+
+// generator example - need to have star symbol to mark as a generator
+function *sayNames () {
+
+  yield "Jack";
+  yield "Jill";
+  yield "John";
+
+}
+
+const nameGen = sayNames();
+
+log(nameGen);
+log(nameGen.next()); // each time we call next we get an object ({value: "Jack", done: false})
+log(nameGen.next().value);
+log(nameGen.next().value);
+log(nameGen.next()); // { done: true }
+
+// id creator - generator 
+function *createIds () {
+
+  let index = 1;
+  while (true) {
+
+    yield index++;
+
+  }
+
+}
+
+// this line essentially instantiates our generator
+const gen = createIds();
+// each time we call it we get a new id
+log(gen.next().value);
