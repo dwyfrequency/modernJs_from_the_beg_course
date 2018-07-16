f9e8817a38ecb0ec3fc55c72be014509bf0cfa57
dwyfrequency <jdwy215@gmail.com>
2018-07-16 09:42:38 -0400

    updating quotes

--- section_8/githubFinder/app.js
+++ section_8/githubFinder/app.js
@@ -6,21 +6,21 @@ const github = new Github();
 const ui = new UI();
 
 // Search input 
-const searchUser = document.querySelector('#searchUser');
+const searchUser = document.querySelector("#searchUser");
 
 // Search input event listener 
-searchUser.addEventListener('keyup', e => {
+searchUser.addEventListener("keyup", e => {
   // Get input text
   const userText = e.target.value;
 
-  if(userText !== '') {
+  if(userText !== "") {
     github
       .getUser(userText)
       .then(data => {
         // log(data); // object of our user profile and their repos
-        if(data.profile.message === 'Not Found') {
+        if(data.profile.message === "Not Found") {
           // Show alert
-          ui.showAlert(`User not found`, `alert alert-danger`);
+          ui.showAlert("User not found", "alert alert-danger");
         } else {
           // Show profile
           ui.showProfile(data.profile);
