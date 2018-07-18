e2eb6b0546d75e14ba5fd8debe92e4015f3092cf
dwyfrequency <jdwy215@gmail.com>
2018-07-18 07:47:55 -0400

    adding basic outline off our js

---
+++ section-12-tracalorie/app.js
@@ -0,0 +1,21 @@
+const log = console.log;
+
+// : Storage Controller
+
+
+// : Item Controller
+const ItemCtrl = (function name(params) {
+  log("item controller");
+}());
+
+// when using iife with es6 place the paranths() outside of the wrapping parens like below
+// const itemCtrl = (() => {
+//   log("item controller");
+// })();
+
+// : UI Controller
+
+
+// : APP Controller
+
+
