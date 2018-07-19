2f0e6a6e86be21edd0ceb7818c72bce7d35807bd
dwyfrequency <jdwy215@gmail.com>
2018-07-19 06:34:40 -0400

    initial app controllers

--- section-12-tracalorie/app.js
+++ section-12-tracalorie/app.js
@@ -5,17 +5,42 @@ const log = console.log;
 
 // : Item Controller
 const ItemCtrl = (function name(params) {
-  log("item controller");
+
+  // : Item Constructor
+  const Item = (id, name, calories) => {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  } 
+
+  // : Data Structure / State
+  const data = {
+    items: [
+      {id: 0, name: "Salmon Dinner", calories: 850},
+      {id: 1, name: "Cookie", calories: 400},
+      {id: 1, name: "Eggs", calories: 300}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
 }());
 
-// when using iife with es6 place the paranths() outside of the wrapping parens like below
+
 // const itemCtrl = (() => {
 //   log("item controller");
 // })();
 
 // : UI Controller
+const UICtrl = (function () {
 
+}());
 
 // : APP Controller
+const AppCtrl = ((ItemCtrl, UICtrl) => {
+
+})(ItemCtrl, UICtrl);
 
 
+
+// when using iife with es6 place the paranths() outside of the wrapping parens like above
